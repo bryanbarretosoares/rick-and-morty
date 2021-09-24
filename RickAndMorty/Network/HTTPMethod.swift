39ce32393d5b301b12cc2a63f5ce13929e0a7e66
Bryan Barreto <bryan.soares@picpay.com>
2021-09-24 18:48:25 -0300

    create http method enum

---
+++ RickAndMorty/Network/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  RickAndMorty
+//
+//  Created by Bryan Barreto Soares on 24/09/21.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
